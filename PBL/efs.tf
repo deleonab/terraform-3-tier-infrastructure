1e1daeb9b84c49136d2a0ac5dfd411604b966896
Dele Onabowu <delesodade@hotmail.com>
2023-01-28 23:34:05 +0000

    Terraform configuration for 3 tier infrastructure multi AZ

--- PBL/efs.tf
+++ PBL/efs.tf
@@ -30,17 +30,12 @@ resource "aws_efs_file_system" "ACS-efs" {
   encrypted  = true
   kms_key_id = aws_kms_key.ACS-kms.arn
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "ACS-efs"
-    },
-  )
+ tags = {Name = "efs-${local.name}"}
 }
 
 # set first mount target for the EFS 
 resource "aws_efs_mount_target" "subnet-1" {
-  file_system_id  = aws_efs_file_system.ACS-efs.id
+  file_system_id  = aws_efs_file_system.ACS-efs.idh
   subnet_id       = aws_subnet.private[2].id
   security_groups = [aws_security_group.datalayer-sg.id]
 }
